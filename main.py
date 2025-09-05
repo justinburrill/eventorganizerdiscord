2c7e53ef944805b6879deea6e31a09e184d952c4
Justin Burrill <me@justinburrill.ca>
2025-09-04 21:33:38 -0400

    update annotations & fix line endings

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import discord
 from command_handlers import func_map, PREFIX
 import json
+from collections.abc import Callable
 
 file = open("info.json", "r")
 SECRET_TOKEN = json.load(file)["secret"]
@@ -15,11 +16,11 @@ client = discord.Client(intents=intents)
 
 
 async def parse_command(message):
-    command: str = message.content.removeprefix(PREFIX).split(" ")[0]
-    args: str = message.content.removeprefix(PREFIX).removeprefix(command).strip()
-    f: callable = func_map.get(command)
+    command: str = message.content.removeprefix(PREFIX).split(" ")[0].lower()
+    args: str = message.content.removeprefix(PREFIX).removeprefix(command).strip().lower()
+    f: Callable[[str], None] | None = func_map.get(command)
     if f is None:
-        is_match: [bool] = list(map(lambda k: k.startswith(command), keys := list(func_map.keys())))
+        is_match: list[bool] = list(map(lambda k: k.startswith(command), keys := list(func_map.keys())))
         matched_commands = [keys[i] for i in range(len(is_match)) if is_match[i]]
         if (count := is_match.count(True)) == 1:  # found command!
             f = func_map.get(matched_commands[0])
@@ -27,7 +28,8 @@ async def parse_command(message):
             return await message.channel.send(f"Ambiguous command: \"{command}\" ({", ".join(matched_commands)})")
         else:
             return await message.channel.send(f"huh? what does that mean?")
-
+    if f is None:
+        raise TypeError("")
     _ = await f(message, args)  # call the handle command function
 
 
@@ -41,7 +43,12 @@ async def on_message(message: discord.Message):
     if message.author == client.user:  # skip if I sent this message
         return
     if message.content.startswith(PREFIX):
-        await parse_command(message)
+        try:
+            await parse_command(message)
+        except BaseException as e:
+            print(f"failed to parse command: {e}")
+            await message.reply("failed to parse command. sorry.")
+
 
 
 def main():
